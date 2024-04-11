e3d8e087be60599ab5dc4f1ecfd99f5f909dd6fe
Ferdinando Rainert <ferdinandorainert24@gmail.com>
2024-04-10 21:03:54 -0300

    adicionando o projeto

---
+++ mvcBean/src/main/java/br/com/mvcBean/controller/PessoaBean.java
@@ -0,0 +1,47 @@
+package br.com.mvcBean.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.mvcBean.model.Pessoa;
+
+@Named("pessoaBean")
+@SessionScoped
+public class PessoaBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Pessoa pessoa;
+	
+	List<Pessoa> pessoas = new ArrayList<Pessoa>();
+	
+	public String adicionar() {
+		pessoas.add(pessoa);
+		pessoa = new Pessoa();
+		System.out.println("Tamanho da lista: " + pessoas.size());
+		return null;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+	
+}
---
+++ mvcBean/src/main/java/br/com/mvcBean/model/Pessoa.java
@@ -0,0 +1,61 @@
+package br.com.mvcBean.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nome;
+	private String profissao;
+	
+	public Pessoa() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getProfissao() {
+		return profissao;
+	}
+
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return id == other.id;
+	}
+	
+	
+
+}
